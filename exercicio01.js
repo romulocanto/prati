e6ffc5883233dcd7954f0979d4613d6e77575a79
romulocanto <romulocanto@gmail.com>
2025-05-17 18:05:17 -0300

    Delete exercicio01.js

--- exercicio01.js
+++
@@ -1,9 +0,0 @@
-// 1. Escreva um programa que recebe um número inteiro e verifica se ele é par ou ímpar utilizando uma estrutura de controle if.
-
-const prompt = require ("prompt-sync") () // Habilitando entrada
-let numero = Number (prompt ("Digite o número: "))
-if (numero % 2 == 0){
-    console.log ("O número é par")
-}else {
-    console.log ("O número é ímpar")
-}
